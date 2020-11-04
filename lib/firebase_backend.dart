e5302376b0f5a76547e2117c6b1566e1024da316
AmanMulani <amanmulani369@gmail.com>
2020-11-04 16:55:46 +0530

    CRUD Operations

---
+++ lib/firebase_backend.dart
@@ -0,0 +1,26 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class FirebaseBackend {
+
+  FirebaseFirestore _firebaseFirestore = FirebaseFirestore.instance;
+
+  createTodoItem(String title) async{
+    await _firebaseFirestore.collection('tasks').add({
+      'title': title,
+      'isDone': false,
+    });
+  }
+
+  updateTodoItem(bool isDone, String uid) async{
+    await _firebaseFirestore.collection('tasks').doc(uid).update(
+      {
+        'isDone': isDone,
+      }
+    );
+  }
+
+  deleteTodoItem(String uid) async {
+    await _firebaseFirestore.collection('tasks').doc(uid).delete();
+  }
+
+}
